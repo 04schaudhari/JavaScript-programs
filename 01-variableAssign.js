204e4ec0013d4192df81a82d6cd26fde86c16cfe
04schaudhari <mandesayali04@gmail.com>
2024-07-31 22:36:39 +0530

    31-7-2024
    Assignment
    01-VariableAssignment
    02-TypeOF

---
+++ 01-variableAssign.js
@@ -0,0 +1,39 @@
+console.log("\n================Step 1==============\n");
+var myFullName;
+console.log("full name  before initialiazation is:",myFullName);
+myFullName="Sayali Chaudhari";
+console.log("First and last name after initialization is:",myFullName);
+
+
+// update My Full Name variable
+
+var myFullName="Sayali Sagar Chaudhari";
+console.log("full name value After Updated:",myFullName);
+
+console.log("\n================Step 2==============\n");
+var myLove="I Love Only JavaScript";
+console.log(myLove);
+
+console.log("\n================Step 3==============\n");
+
+var firstName="Sayali";
+console.log("First Name is:",firstName);
+
+var lastName="Chaudhari";
+console.log("Last Name is:",lastName);
+
+var age=30;
+console.log("My Age is:",age);
+
+var address="Pune";
+
+console.log("Address is:",address);
+
+var pinCode=411061;
+console.log("Pin Code is:",pinCode);
+
+var gender="Female";
+console.log("Gender is:",gender);
+
+var isMarried="Yes";
+console.log("Marriege Status is:",isMarried);
---
+++ 02-typeOf.js
@@ -0,0 +1,39 @@
+console.log("\n==============Step 1==============\n");
+var bankName;
+console.log("Bank name is:",bankName);
+var bankNametypeOf=typeof bankName;
+console.log("Datatype of Bank Name is:",bankNametypeOf);
+
+
+var bankName="SBI bank";
+console.log("Bank name is:",bankName);
+var bankNametypeOf=typeof bankName;
+console.log("Type of Datatype after initialization:",bankNametypeOf);
+
+console.log("\n==============Step 2==============\n");
+var marks="90%";
+console.log("The mark is:",marks);
+var typeofmark=typeof marks;
+console.log("The datatype of mark is:",typeofmark);
+
+console.log("\n==============Step 3==============\n");
+var isWorking;
+console.log("The value of isWorking before imitialization:",isWorking);
+var isWorkingtype=typeof isWorking;
+console.log("The DatatypeisWorking before imitialization:",isWorkingtype);
+var isWorking=true;
+
+console.log("the value of isWorking after imitialization:",isWorking);
+var isWorkingtype=typeof isWorking;
+console.log("The DatatypeisWorking after imitialization:",isWorkingtype);
+
+
+console.log("\n============== Step 4==============\n");
+var totalCount="Hundred and Seven";
+console.log("Value of total count is:",totalCount);
+var totalCounttypeof=typeof totalCount;
+console.log('The Datatype of total count is:',totalCounttypeof);
+var totalCount=107;
+console.log("Value of total count is:",totalCount);
+var totalCounttypeof=typeof totalCount;
+console.log('The Datatype of total count is:',totalCounttypeof);
