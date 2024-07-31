8536f46ae2cbdd32cb51d4bc111e5373faf38b93
04schaudhari <mandesayali04@gmail.com>
2024-07-31 21:52:06 +0530

    30-7-2024 Assignment
    01-variable-assignment

---
+++ 01-variable-assignment.js
@@ -0,0 +1,17 @@
+console.log("City Name is: Pune");
+
+console.log("-------------------------------------");
+
+console.log("Best Friend Name is: Pooja");
+
+console.log("-------------------------------------");
+
+console.log("Country Name is: India");
+
+console.log("-------------------------------------");
+
+console.log("School Name: P.E.S Girls High School Modern Pune");
+
+console.log("-------------------------------------");
+
+console.log("College Name is: Modern College Pune");
