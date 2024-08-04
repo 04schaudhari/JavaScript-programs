29e108c6e5f9bf3638c2ae6fa665bb3caac479d9
04schaudhari <mandesayali04@gmail.com>
2024-08-04 18:12:44 +0530

    Function Assignment
    2-8-2024

---
+++ 03-funAssig01.js
@@ -0,0 +1,36 @@
+
+console.log("==================Step 1==================\n");
+function message(){
+
+    console.log("I love JavaScript");
+    console.log("I Love React JS");
+
+}
+
+message();
+
+console.log("\n==================Step 2==================\n");
+
+function personalDetails(firstName, lastName, collegeName){
+
+ var firstName = "Sayali";
+ var lastName = "Chaudhari";
+ var collegeName = "Modern College Pune"
+
+    console.log("My First Name is:", firstName);
+    console.log("My Last Name is:", lastName);
+    console.log("My College Name is:", collegeName);
+}
+
+personalDetails();
+
+
+console.log("\n==================Step 3==================\n");
+
+function addthreevalues(n1, n2, n3){
+    var add = n1 + n2 + n3;
+    console.log("Addition of Three Value is:",add);
+    console.log("Addition of Value is:","Hello", "Good", "Morning")
+
+}
+addthreevalues(10.23, 600, 40);
---
+++ 03-funAssig02.js
@@ -0,0 +1,16 @@
+console.log("\n==================Step 1==================\n");
+
+function bankDetails(bankName, accountNum, location, pincode){
+
+    console.log("Bank Name is:",bankName);
+    console.log("Bank Account Number is:",accountNum);
+    console.log("Bank Location is:",location);
+    console.log("Bank Pin Code is:", pincode);  
+    console.log("\n");
+
+}
+bankDetails("CITI Bank", 3456782345, "Pune", 431202);
+bankDetails("Axis Bank", 7856782345, "Mumbai", 441202);
+bankDetails("HDFC Bank", 8956782345, "Pune", 631202, "Open");
+
+
